814939166619530c27a8e00891f87fe4c6c93460
hanmiton <hanmilton_12@outlook.com>
2016-06-17 15:58:58 -0500

    filtro imagenes

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,11 +1,21 @@
 (function () {
 
   angular.module('ingedex.filters', [])
-    .filter('imageify', function () {
+    .filter('normalize', function () {
+      return function (input) {
+          input = input
+                  .replace('♀', 'f')
+                  .replace('♂', 'm')
+                  .replace(/\W+/g, "");
+          return input.toLowerCase();
+      };
+    })
+
+    .filter('imageify', ['$filter', function ($filter) {
     return function (input) {
-      var url = "img/ingenieros/" + input.toLowerCase() + ".jpg";
+      var url = "img/ingenieros/" + $filter('normalize')(input) + ".jpg";
       return url;
     };
-  });
+  }]);
 
 })();
