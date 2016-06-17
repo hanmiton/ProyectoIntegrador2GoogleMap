e7639361911a6ea7064e7752ece2e4a2e668bba3
hanmiton <hanmilton_12@outlook.com>
2016-06-17 02:21:42 -0500

    añadiendo habilidades y evolucion ingenieor

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,22 @@ var app = angular.module('Ingedex', ['google-maps']);
 app.controller('IngenieroController', function () {
     this.ingeniero = {
       id: "001",
-      name: "nombre001",
+      name: "Nombre001",
       ingenieria: "Ingenieria001",
       tipo: [ "Tipo1", "Tipo2" ],
       edad: "40",
       facultad: "facultad001",
-      habilidades: [ "habilidad1", "habilidad2"]
+      habilidades: [ "habilidad1", "habilidad2"],
+       stats: {
+        st1: 45,
+        st2: 49,
+        st3: 49,
+        "sp.st4": 65,
+        "sp.st5": 65,
+        st6: 45,
+        total: 318
+      },
+      evolution: [ "Ingeniero", "Masterado", "Doctorado" ]
     };
   });
 
