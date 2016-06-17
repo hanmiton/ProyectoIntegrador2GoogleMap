a47014ff4067865707b79fe4a04b7b46cc15d7aa
hanmiton <hanmilton_12@outlook.com>
2016-06-17 03:57:13 -0500

    partial solicitud

--- public/js/app.js
+++ public/js/app.js
@@ -33,28 +33,8 @@ app.controller('IngenieroController', function () {
 
   });
 
-  app.controller('SolicitudesController', function () {
-    this.solicitudes = [];
-    this.solicitud = {};
-    this.show = false;
+ 
 
-    this.toggle = function () {
-      this.show = !this.show;
-    };
-
-    this.anonymousChanged = function () {
-      if (this.solicitud.anonymous) {
-        this.solicitud.email = "";
-      }
-    };
-
-    this.addSolicitud = function () {
-      this.solicitud.date = Date.now();
-      this.solicitudes.push(this.solicitud);
-      this.solicitud = {};
-    };
-
-  });
   app.filter('imageify', function () {
     return function (input) {
       var url = "img/ingenieros/" + input.toLowerCase() + ".jpg";
@@ -94,6 +74,35 @@ app.controller('IngenieroController', function () {
     return {
       retrict: 'E',
       templateUrl: 'partials/ingeniero-evolution.html'
+    };
+  });
+
+   app.directive('ingenieroSolicitudes', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/ingeniero-solicitudes.html',
+      controller: function () {
+         this.solicitudes = [];
+          this.solicitud = {};
+          this.show = false;
+
+          this.toggle = function () {
+            this.show = !this.show;
+          };
+
+          this.anonymousChanged = function () {
+            if (this.solicitud.anonymous) {
+              this.solicitud.email = "";
+            }
+          };
+
+          this.addSolicitud = function () {
+            this.solicitud.date = Date.now();
+            this.solicitudes.push(this.solicitud);
+            this.solicitud = {};
+        };
+      },
+      controllerAs: 'sltsCtrl'
     };
   });
 
