0b9e9ff0f5533c963357ddc04a8bf4cdff0ec612
hanmiton <hanmilton_12@outlook.com>
2016-06-17 18:34:25 -0500

    underscore añdido y mejorando tarjeta ingeniero

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,20 +1,29 @@
-(function () {
+(function (_) {
 
   angular.module('ingedex.controllers', [])
-      .controller('IngedexController', ['$scope', '$routeParams', 'ingenieroService', function ($scope, $routeParams, ingenieroService) {
+    .controller('IngedexController', ['$scope', '$routeParams', 'ingenieroService', function ($scope, $routeParams, ingenieroService) {
       var type = $routeParams.type;
 
       if (type) {
         $scope.type = type;
 
         ingenieroService.byType(type).then(function (data) {
-          $scope.ingenieros = data;
+          $scope.ingenieros = data
+          $scope.groupped = partition(data, 4);
         });
       } else {
         ingenieroService.all().then(function (data) {
           $scope.ingenieros = data;
+          $scope.groupped = partition(data, 4);
         });
       }
+
+      function partition(data, n) {
+        return _.chain(data).groupBy(function (element, index) {
+          return Math.floor(index / n);
+        }).toArray().value();
+      }
+
     }])
 
     .controller('IngenieroController', ['$scope', '$routeParams', 'ingenieroService', function ($scope, $routeParams, ingenieroService) {
@@ -144,4 +153,4 @@
 
 
 
-})();
+})(_);
