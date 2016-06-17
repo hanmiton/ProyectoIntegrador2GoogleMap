8366d5608b373c6f7f16df2503af8b4b5dc94da1
hanmiton <hanmilton_12@outlook.com>
2016-06-17 02:08:35 -0500

    primera version proyecto con google map

---
+++ public/js/app.js
@@ -0,0 +1,127 @@
+var app = angular.module('Ingedex', ['google-maps']);
+
+app.controller('IngenieroController', function () {
+    this.ingeniero = {
+      id: "001",
+      name: "nombre001",
+      ingenieria: "Ingenieria001",
+      tipo: [ "Tipo1", "Tipo2" ],
+      edad: "40",
+      facultad: "facultad001",
+      habilidades: [ "habilidad1", "habilidad2"]
+    };
+  });
+
+app.factory('MarkerCreatorService', function () {
+
+    var markerId = 0;
+
+    function create(latitude, longitude) {
+        var marker = {
+            options: {
+                animation: 1,
+                labelAnchor: "28 -5",
+                labelClass: 'markerlabel'    
+            },
+            latitude: latitude,
+            longitude: longitude,
+            id: ++markerId          
+        };
+        return marker;        
+    }
+
+    function invokeSuccessCallback(successCallback, marker) {
+        if (typeof successCallback === 'function') {
+            successCallback(marker);
+        }
+    }
+
+    function createByCoords(latitude, longitude, successCallback) {
+        var marker = create(latitude, longitude);
+        invokeSuccessCallback(successCallback, marker);
+    }
+
+    function createByAddress(address, successCallback) {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'address' : address}, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                var firstAddress = results[0];
+                var latitude = firstAddress.geometry.location.lat();
+                var longitude = firstAddress.geometry.location.lng();
+                var marker = create(latitude, longitude);
+                invokeSuccessCallback(successCallback, marker);
+            } else {
+                alert("Unknown address: " + address);
+            }
+        });
+    }
+
+    function createByCurrentLocation(successCallback) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var marker = create(position.coords.latitude, position.coords.longitude);
+                invokeSuccessCallback(successCallback, marker);
+            });
+        } else {
+            alert('Unable to locate current position');
+        }
+    }
+
+    return {
+        createByCoords: createByCoords,
+        createByAddress: createByAddress,
+        createByCurrentLocation: createByCurrentLocation
+    };
+
+});
+
+app.controller('MapCtrl', ['MarkerCreatorService', '$scope', function (MarkerCreatorService, $scope) {
+
+        MarkerCreatorService.createByCoords(40.454018, -3.509205, function (marker) {
+            marker.options.labelContent = 'Autentia';
+            $scope.autentiaMarker = marker;
+        });
+        
+        $scope.address = '';
+
+        $scope.map = {
+            center: {
+                latitude: $scope.autentiaMarker.latitude,
+                longitude: $scope.autentiaMarker.longitude
+            },
+            zoom: 12,
+            markers: [],
+            control: {},
+            options: {
+                scrollwheel: false
+            }
+        };
+
+        $scope.map.markers.push($scope.autentiaMarker);
+
+        $scope.addCurrentLocation = function () {
+            MarkerCreatorService.createByCurrentLocation(function (marker) {
+                marker.options.labelContent = 'You´re here';
+                $scope.map.markers.push(marker);
+                refresh(marker);
+            });
+        };
+        
+        $scope.addAddress = function() {
+            var address = $scope.address;
+            if (address !== '') {
+                MarkerCreatorService.createByAddress(address, function(marker) {
+                    $scope.map.markers.push(marker);
+                    refresh(marker);
+                });
+            }
+        };
+
+        function refresh(marker) {
+            $scope.map.control.refresh({latitude: marker.latitude,
+                longitude: marker.longitude});
+        }
+
+    }]);
+
+
