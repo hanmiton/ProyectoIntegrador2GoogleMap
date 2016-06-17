6818e59f0e19e0ca2ccf6cc3f67d3ad53dc24ddc
hanmiton <hanmilton_12@outlook.com>
2016-06-17 04:12:36 -0500

    añandiendo diretives.js yfirlteres.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,80 +1,10 @@
 (function () {
 var app = angular.module('ingedex', [
     'google-maps',
-    'ingedex.controllers'
+    'ingedex.controllers',
+    'ingedex.directives',
+    'ingedex.filters'
     ]);
-
-app.filter('imageify', function () {
-    return function (input) {
-      var url = "img/ingenieros/" + input.toLowerCase() + ".jpg";
-      return url;
-    };
-  });
-
-  app.directive('ingenieroName', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/ingeniero-name.html'
-    };
-  });
-
-  app.directive('ingenieroImage', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/ingeniero-image.html'
-    };
-  });
-
-  app.directive('ingenieroData', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/ingeniero-data.html'
-    };
-  });
-
-   app.directive('ingenieroStats', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/ingeniero-stats.html'
-    };
-  });
-
-   app.directive('ingenieroEvolution', function () {
-    return {
-      retrict: 'E',
-      templateUrl: 'partials/ingeniero-evolution.html'
-    };
-  });
-
-   app.directive('ingenieroSolicitudes', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/ingeniero-solicitudes.html',
-      controller: function () {
-         this.solicitudes = [];
-          this.solicitud = {};
-          this.show = false;
-
-          this.toggle = function () {
-            this.show = !this.show;
-          };
-
-          this.anonymousChanged = function () {
-            if (this.solicitud.anonymous) {
-              this.solicitud.email = "";
-            }
-          };
-
-          this.addSolicitud = function () {
-            this.solicitud.date = Date.now();
-            this.solicitudes.push(this.solicitud);
-            this.solicitud = {};
-        };
-      },
-      controllerAs: 'sltsCtrl'
-    };
-  });
-
 
 app.factory('MarkerCreatorService', function () {
 
---
+++ public/js/filters.js
@@ -0,0 +1,11 @@
+(function () {
+
+  angular.module('ingedex.filters', [])
+    .filter('imageify', function () {
+    return function (input) {
+      var url = "img/ingenieros/" + input.toLowerCase() + ".jpg";
+      return url;
+    };
+  });
+
+})();
